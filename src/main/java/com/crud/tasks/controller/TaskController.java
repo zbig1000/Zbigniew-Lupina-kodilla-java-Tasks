07746807e2cf0e3516fcac5a3cd85f80051d74f6
zbig1000 <zbig.lupina@gmail.com>
2018-02-10 19:50:05 +0100

    15.2 Lesson - Rest API 2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,18 +1,25 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "/getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/getTask/{taskId}")
@@ -40,12 +47,7 @@ public class TaskController {
             value = "createTaskDirectly",
             params = {"taskId"}
     )
-    public TaskDto createTaskDirectly(@RequestParam() Long taskId)
-{
-        Long newID = new Long(taskId);
-        return new TaskDto(newID, "title", "content ok");
-
+    public TaskDto createTaskDirectly(@RequestParam() Long taskId) {
+        return new TaskDto(taskId, "title", "content ok");
     }
-
-
 }
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.domain.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
+}
