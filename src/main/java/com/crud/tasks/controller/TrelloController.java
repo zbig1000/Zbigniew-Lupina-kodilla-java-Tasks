6616ceeeff88b2b8824005adbf2b7f6228a66ca9
zbig1000 <zbig.lupina@gmail.com>
2018-03-03 18:03:37 +0100

    18.3 Lesson - Expanding REST client

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -22,14 +22,11 @@ public class TrelloController {
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
         trelloBoards.forEach(trelloBoardDto -> {
-
             System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
             System.out.println("This board contains lists: ");
 
             trelloBoardDto.getLists().forEach(trelloList ->
                     System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
         });
 
     }
