2fcdb533392ac5a74f691c04656394cb1689e205
zbig1000 <zbig.lupina@gmail.com>
2018-03-03 23:25:54 +0100

    18.3 Task - Expanding REST clinet - fields in JSON

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -5,10 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+import java.util.Map;
+
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
+
+    private String votes;
+    private String board;
+    private String card;
 
     @JsonProperty("id")
     private String id;
@@ -18,4 +25,14 @@ public class CreatedTrelloCard {
 
     @JsonProperty("shortUrl")
     private String shortUrl;
+
+    @SuppressWarnings("unchecked")
+    @JsonProperty("badges")
+    private void unpackNested(Map<String,Object> badges) {
+        this.votes = badges.get("votes").toString();
+        Map<String,Object> attachmentsByType = (Map<String,Object>)badges.get("attachmentsByType");
+        Map<String,Object> trello = (Map<String,Object>)attachmentsByType.get("trello");
+        this.board = trello.get("board").toString();
+        this.card = trello.get("card").toString();
+    }
 }
