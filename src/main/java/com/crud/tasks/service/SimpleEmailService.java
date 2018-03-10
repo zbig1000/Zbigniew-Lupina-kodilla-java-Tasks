e49ca32c4353290c8669264bc7680011d0ca29cd
zbig1000 <zbig.lupina@gmail.com>
2018-03-10 17:21:33 +0100

    19.3 Lesson - sending emails

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleEmailService {
+
+    @Autowired
+    private  JavaMailSender javaMailSenderSender;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+    public void send(final Mail mail) {
+        LOGGER.info("starting email preparation...");
+        try {
+            javaMailSenderSender.send(createMailMessage(mail));
+            LOGGER.info("Email has beeen sent.");
+        } catch (MailException e) {
+            LOGGER.error("failed email sending ", e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        return mailMessage;
+    }
+
+}
