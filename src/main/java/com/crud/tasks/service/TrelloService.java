2b46098662235d1faf5475d31ec41ea525b3e686
zbig1000 <zbig.lupina@gmail.com>
2018-03-10 20:03:25 +0100

    19.4 Lesson - Mails in project

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrelloService {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+
+        emailService.send(new Mail(
+                "",
+                "Tasks: New Trello card",
+                "New card: " + trelloCardDto.getName() + " "
+        ));
+
+        return newCard;
+    }
+}
