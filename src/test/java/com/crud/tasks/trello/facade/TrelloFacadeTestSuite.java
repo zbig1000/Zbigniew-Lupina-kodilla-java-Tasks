08ec44d7767c32394285f199e02ec3cf5dfac024
zbig1000 <zbig.lupina@gmail.com>
2018-03-26 11:26:02 +0200

    21.1 private test Facade

--- src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
+++ src/test/java/com/crud/tasks/trello/facade/TrelloFacadeTestSuite.java
@@ -20,7 +20,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
+//private file
 @RunWith(MockitoJUnitRunner.class)
 public class TrelloFacadeTestSuite {
 
